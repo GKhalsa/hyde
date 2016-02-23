c1893e257197a53b5a26a5c1b94d1620d891f18c
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-22 19:01:31 -0700

    iteration 0 working with argument error test

--- test/hyde_test.rb
+++ test/hyde_test.rb
@@ -7,9 +7,11 @@ class HydeTest < Minitest::Test
   def test_when_new_is_argv0_it_creates_new_file_structure
     hyde = Hyde.new
     file_path = "/Users/Gurusundesh/turing/1module/hyde/test_files"
+
     if Dir.exist?("#{file_path}")
       FileUtils.remove_dir("#{file_path}")
     end
+
     hyde.create_tree("#{file_path}")
     assert Dir.exist?("#{file_path}/source")
     assert Dir.exist?("#{file_path}/source/css")
@@ -20,13 +22,22 @@ class HydeTest < Minitest::Test
   def test_when_create_tree_is_called_the_files_are_created
     hyde = Hyde.new
     file_path = "/Users/Gurusundesh/turing/1module/hyde/test_files"
+
     if Dir.exist?("#{file_path}")
       FileUtils.remove_dir("#{file_path}")
     end
+
     hyde.create_tree("#{file_path}")
     assert File.exist?("#{file_path}/source/css/main.css")
     assert File.exist?("#{file_path}/source/index.md")
     assert File.exist?("#{file_path}/source/about.md")
     assert File.exist?("#{file_path}/source/posts/welcome_to_hyde.md")
+  end
+
+  def test_argument_error_activates_when_directories_previously_exist
+    hyde = Hyde.new
+    file_path = "/Users/Gurusundesh/turing/1module/hyde/test_files"
+
+    assert_raises(ArgumentError) { hyde.create_tree("#{file_path}") }
   end
 end
