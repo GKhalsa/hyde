a7bfc9ce80ce662d774867896cf017d881fd2ece
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-22 18:52:11 -0700

    iteration 0 complete

--- lib/hyde.rb
+++ lib/hyde.rb
@@ -1,12 +1,17 @@
 class Hyde
 
   def create_tree(file_path)
-    Dir.mkdir("#{file_path}")
-    create_dirs(file_path)
-    create_files(file_path)
+    if Dir.exist?("#{file_path}")
+      raise ArgumentError.new("this path already exists!")
+    else
+      Dir.mkdir("#{file_path}")
+      create_dirs(file_path)
+      create_files(file_path)
+    end
   end
 
   def create_dirs(file_path)
+    Dir.mkdir("#{file_path}/output")
     Dir.mkdir("#{file_path}/source")
     Dir.mkdir("#{file_path}/source/css")
     Dir.mkdir("#{file_path}/source/pages")
