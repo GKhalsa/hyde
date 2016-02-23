37ccfd691edebd59aa95d7073226fdd2e380f649
Jonathan Bernesser <jbern16@gmail.com>
2016-02-23 14:36:47 -0700

    added set_up

--- test/structure_test.rb
+++ test/structure_test.rb
@@ -1,12 +1,21 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/hyde'
+require_relative '../lib/structure'
 require 'fileutils'
 
 class StructureTest < Minitest::Test
+
+  attr_reader :structure,
+              :test_path,
+              :file_path
+
+  def setup
+    @structure = Structure.new
+    @test_path = Dir.home
+    @file_path = "test_path"
+  end
+
   def test_when_new_is_argv0_it_creates_new_file_structure
-    structure = Structure.new
-    file_path = "/Users/Gurusundesh/turing/1module/hyde/test_files"
 
     if Dir.exist?("#{file_path}")
       FileUtils.remove_dir("#{file_path}")
@@ -20,8 +29,6 @@ class StructureTest < Minitest::Test
   end
 
   def test_when_create_tree_is_called_the_files_are_created
-    structure = Structure.new
-    file_path = "/Users/Gurusundesh/turing/1module/structure/test_files"
 
     if Dir.exist?("#{file_path}")
       FileUtils.remove_dir("#{file_path}")
@@ -31,13 +38,10 @@ class StructureTest < Minitest::Test
     assert File.exist?("#{file_path}/source/css/main.css")
     assert File.exist?("#{file_path}/source/index.md")
     assert File.exist?("#{file_path}/source/about.md")
-    assert File.exist?("#{file_path}/source/posts/welcome_to_structure.md")
+    assert File.exist?("#{file_path}/source/posts/welcome_to_hyde.md")
   end
 
   def test_argument_error_activates_when_directories_previously_exist
-    structure = Structure.new
-    file_path = "/Users/Gurusundesh/turing/1module/hyde/test_files"
-
     assert_raises(ArgumentError) { structure.create_tree("#{file_path}") }
   end
 end
