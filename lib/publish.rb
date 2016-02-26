6288ca4358847862dbd9a22d42864ddf8a2fb23b
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-25 18:11:05 -0700

    changed publish format

--- lib/publish.rb
+++ lib/publish.rb
@@ -1,10 +1,13 @@
 module Publish
 
-def publish_to_git_page(file_path)
-  if File.exists?("#{file_path}/../index.html")
-     File.delete("#{file_path}/../index.html")
+  def publish_to_git_page(file_path)
+    if File.exists?("#{file_path}/../index.html")
+       File.delete("#{file_path}/../index.html")
+    end
+    copy_index = "cd #{file_path}/output && cp index.html #{file_path}/.. "
+    move_to_proper_dir = "&& cd #{file_path}/.. && git add . "
+    commit = "&& git commit -m 'updated' && git push origin master"
+    `#{copy_index}#{move_to_proper_dir}#{commit}`
   end
-`cd #{file_path}/output && cp index.html #{file_path}/.. && cd #{file_path}/.. && git add . && git commit -m 'updated' && git push origin master`
-end
 
 end
